08dc1b9c8ec3cd6f63e94332d70180a7d71821f0
LionyxML <rahul.juliato@gmail.com>
2021-05-05 10:03:28 -0300

    Criado o navbar, rotas para views e views básicas.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Navbar/>
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components : {
+    Navbar
+  }
+}
+</script>
+
 <style>
+
 </style>
